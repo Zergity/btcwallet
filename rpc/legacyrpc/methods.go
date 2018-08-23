e871fd83f6733dc98d36ecffff28bfb7cf5ffab2
Zergity <Zergity@gmail.com>
2018-08-23 10:54:21 +0700

    Remove param.

--- rpc/legacyrpc/methods.go
+++ rpc/legacyrpc/methods.go
@@ -442,7 +442,7 @@ func getBalance(icmd interface{}, w *wallet.Wallet) (interface{}, error) {
 		accountName = *cmd.Account
 	}
 	if accountName == "*" {
-		balance, err = w.CalculateBalance(int32(*cmd.MinConf), cmd.Token == 1)
+		balance, err = w.CalculateBalance(int32(*cmd.MinConf), false)
 		if err != nil {
 			return nil, err
 		}
@@ -490,8 +490,6 @@ func getBlockCount(icmd interface{}, w *wallet.Wallet) (interface{}, error) {
 // information about the current state of btcwallet.
 // exist.
 func getInfo(icmd interface{}, w *wallet.Wallet, chainClient *chain.RPCClient) (interface{}, error) {
-	cmd := icmd.(*btcjson.GetBalanceCmd)
-
 	// Call down to btcd for all of the information in this command known
 	// by them.
 	info, err := chainClient.GetInfo()
@@ -499,7 +497,7 @@ func getInfo(icmd interface{}, w *wallet.Wallet, chainClient *chain.RPCClient) (
 		return nil, err
 	}
 
-	bal, err := w.CalculateBalance(1, cmd.Token == 1)
+	bal, err := w.CalculateBalance(1, false)
 	if err != nil {
 		return nil, err
 	}
